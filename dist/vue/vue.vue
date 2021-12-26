c14c898deb74f0fecdead35fe1fbb622a72b082c
Supakit-palanun <you@example.com>
2021-12-26 22:12:03 +0700

    first commit

---
+++ dist/vue/vue.vue
@@ -0,0 +1,11 @@
+var pro1 = new Vue({
+    el: '#pro',
+    data: {
+        profile: 'CSS',
+        name: 'Suphakit palanun',
+        name2: 'Noppadon Homjan',
+        html: 'HTML',
+        btc: 'BOOTSTRAP5',
+        vu: 'VUE'
+    }
+    })
